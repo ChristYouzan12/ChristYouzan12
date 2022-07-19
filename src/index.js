da3474bc9e788c7d13362cd0cca02e778fa46213
Yanisarnold <92054492+Yanisarnold@users.noreply.github.com>
2022-07-19 17:05:51 +0100

    reacts project -2 :qa

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import Tabs from "./tabs";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import Product from "./product--card";
+import Timer from "./countDownTimer";
+import ProductDisplay from "./productDisplay";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Tabs /> */}
+    {/* <Product /> */}
+    <Timer />
+    {/* <ProductDisplay /> */}
   </React.StrictMode>
 );
 
