91a71324e24bcdeaec03a774085f1125a75db72b
Christ Youzan <christ.youzan@JJJJansmacbook.local>
2022-08-02 17:00:34 +0100

    commiting new stuff part 2

--- src/components/webPage2/Header.js
+++ src/components/webPage2/Header.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect, useState}  from "react";
 import "./Header.css";
 import logo from "./Logo.svg";
+import Menu from "./HamburgerMenu.svg"
 export const Header = () => {
+  
+  const media = 1064;
+  const [isMobile, setMobile] = useState(null);
+
+  const handleResize = () =>{
+    setMobile(window.innerWidth <= media);
+  }
+  
+  useEffect(()=> {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize)
+  }, []);
+  
   return (
     <nav className="container">
-      <img src={logo} alt="logo"></img>
-      <div className="mini-container">
+     <img src={logo} alt="logo">
+      </img>
+     { !isMobile ? (<div className="mini-container">
         <ul className="menu">
           <li>
             <a href="blank_" target="blank_">
@@ -24,7 +40,9 @@ export const Header = () => {
           </li>
         </ul>
         <button id="btn">Subscribe</button>
-      </div>
+      </div>   ): (
+    <img src={Menu} alt="Hamburger" className="hamburger"></img>)}
     </nav>
   );
 };
+
