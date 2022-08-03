4c82ce18ac431257b9061f558fc4d67986467455
Yanisarnold <92054492+Yanisarnold@users.noreply.github.com>
2022-08-03 21:41:33 +0100

    progress on responsive

--- src/components/webPage2/Main-component.js
+++ src/components/webPage2/Main-component.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Header } from "./Header";
 import "./Main.css";
 import Main from "./Main";
+import Logo from "./Logo.svg";
 
 const Main_components = () => {
   return (
     <div className="containers">
       <Header />
       <Main />
+      <footer className="footer-content">
+        <img src={Logo} alt="logo"></img>
+        <p>Unifeed © 2017 All Copyrights Not Reserved</p>
+      </footer>
     </div>
   );
 };
