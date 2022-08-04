53a9a7a2860fa2a179fc271b58c46eac637e0aa6
Yanisarnold <92054492+Yanisarnold@users.noreply.github.com>
2022-08-04 23:31:55 +0100

    side Menu useState Improvement

--- src/components/webPage2/Card.js
+++ src/components/webPage2/Card.js
@@ -2,18 +2,21 @@ import React from "react";
 
 const Card = (prop) => {
   return (
-    <div className="items">
-      <img src={prop.article} alt="banner" width="210px"></img>
-      <h2>{prop.title}</h2>
-      <p>{prop.summary}</p>
-      <div className="profile">
-        <img src={prop.user} alt="template" width="40px"></img>
-        <div className="profile-card">
-          <h4>{prop.name}</h4>
-          <p>{prop.job_title}</p>
+    <>
+      <div className="items">
+        <img src={prop.article} alt="banner" width="210px"></img>
+        <h2>{prop.title}</h2>
+        <p>{prop.summary}</p>
+        <div className="profile">
+          <img src={prop.img} alt="template" width="40px"></img>
+
+          <div>
+            <h4>{prop.name}</h4>
+            <p>{prop.job_title}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
